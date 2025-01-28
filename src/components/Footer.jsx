07a673a2977a614f137edea591584fb32c106cac
PG26 Sebastian <sebaatianj16@gmail.com>
2025-01-27 23:56:10 -0800

    clean stuff & depencies

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,14 +7,13 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer-content">
-                <p>&copy; {currentYear} Sebastian Garcia. All rights reserved.</p> {/* Año dinámico */}
+                <p>&copy; {currentYear} Sebastian Garcia. All rights reserved.</p>
                 <ul className="footer-links">
-                    <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a>
                     <a href="https://www.linkedin.com/in/sebastian-garcia-dev" target="_blank" rel="noopener noreferrer" className="social-link">
-                        <i className="fa fa-linkedin"></i> LinkedIn
+                        <i className="fa fa-linkedin"></i>
                     </a>
                     <a href="https://github.com/SzK14" target="_blank" rel="noopener noreferrer" className="social-link">
-                        <i className="fa fa-github"></i> GitHub
+                        <i className="fa fa-github"></i>
                     </a>
                 </ul>
             </div>
