28f3353e05bc56338915497336cee2f107a4d1af
PG26 Sebastian <sebaatianj16@gmail.com>
2025-01-28 23:57:16 -0800

    Home Page & Footer Update

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -6,14 +6,15 @@ const HomePage = () => {
     <div className="home-page">
       <div className="cover-image">
         <div className="fog-effect">
-          <h1>Sebastian Garcia</h1>
-          <p>Web / Mobile / Games</p>
-          <p>Developer</p>
+          <h1 className="title animated-title">Sebastian Garcia</h1>
+          <p className="subtitle animated-text">Web / Mobile / Games</p>
+          <p className="role animated-text">Developer</p>
+
         </div>
       </div>
       <div className="profile-container">
         <div className="profile-info">
-          <p className="bio">
+          <p className="typewriter-text">
             I am a software developer passionate and courius about creating innovative and efficient solutions. 
             I enjoy transforming ideas into functional and attractive web applications.
              My goal is to continue learning and growing in the field of software development, 
@@ -22,45 +23,54 @@ const HomePage = () => {
           <a href="/about" className="btn">About</a>
         </div>
       </div>
-      <div className="skills-section">
-        <h2>Skills</h2>
-        <ul>
-          <li>JavaScript</li>
-          <li>React</li>
-          <li>CSS</li>
-          <li>HTML</li>
-        </ul>
-        <h2>Tools</h2>
-        <ul>
-          <li>Visual Studio Code</li>
-          <li>GitHub</li>
-          <li>Figma</li>
-        </ul>
-      </div>
-      <div className="projects-container">
-        <div className="projects-grid">
-          <div className="project-item">
-            <img src="/src/assets/GameLogo.png" alt="Game" class="ProjectsImg"/>
-            <h3>Abyssal Ascent</h3>
-            <p>2D Sidescroller Game made on Unity</p>
-          </div>
-          <div className="project-item">
-            <img src="/src/assets/GameLogo.png" alt="Game" class="ProjectsImg"/>
-            <h3>Stick Dots</h3>
-            <p>2D Sidescroller Game made on Unity</p>
-          </div>
-          <div className="project-item">
-            <img src="/src/assets/GameLogo.png" alt="Game" class="ProjectsImg"/>
-            <h3>Mountain Pool & Spa</h3>
-            <p>2D Sidescroller Game made on Unity</p>
-          </div>
-          <div className="project-item">
-            <img src="/src/assets/GameLogo.png" alt="Game" class="ProjectsImg"/>
-            <h3>SkyTrain Surfers</h3>
-            <p>2D Sidescroller Game made on Unity</p>
+    
+      <section className="projects-section">
+        <h2 className="projects-title">My Projects</h2>
+        <div className="projects-container">
+          <div className="projects-grid">
+            <div className="project-item">
+              <div className="project-image">
+                <img src="/src/assets/GameLogo.png" alt="Game" className="projects-img" />
+                <div className="hover-overlay">
+                  <a href="#projects" className="about-button">About</a>
+                </div>
+              </div>
+              <h3>Abyssal Ascent</h3>
+              <p>2D Sidescroller Game made on Unity</p>
+            </div>
+            <div className="project-item">
+              <div className="project-image">
+                <img src="/src/assets/GameLogo.png" alt="Game" className="projects-img" />
+                <div className="hover-overlay">
+                  <a href="#projects" className="about-button">About</a>
+                </div>
+              </div>
+              <h3>Stick Dots</h3>
+              <p>Connect lines to make a path for the dots to follow made on Unity</p>
+            </div>
+            <div className="project-item">
+              <div className="project-image">
+                <img src="/src/assets/GameLogo.png" alt="Game" className="projects-img" />
+                <div className="hover-overlay">
+                  <a href="#projects" className="about-button">About</a>
+                </div>
+              </div>
+              <h3>Mountain Pool & Spa</h3>
+              <p>Website for a business</p>
+            </div>
+            <div className="project-item">
+              <div className="project-image">
+                <img src="/src/assets/GameLogo.png" alt="Game" className="projects-img" />
+                <div className="hover-overlay">
+                  <a href="#projects" className="about-button">About</a>
+                </div>
+              </div>
+              <h3>SkyTrain Surfers</h3>
+              <p>2D Endless Runner Game made on Unity</p>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
     </div>
   );
 };
